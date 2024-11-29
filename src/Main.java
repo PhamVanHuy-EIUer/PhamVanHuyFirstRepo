7205d553f8e868cc3bf0a74d63f61afdf2ec8687
PhamVanHuyEIUer <huy.phamvan.cit22@eiu.edu.vn>
2024-11-29 18:25:58 +0700

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 package src;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
         System.out.println("Welcome to Pham Van Huy's Java Reponsitory");
-        System.out.println("Hello <input_name>");
+        System.out.println(name);
         System.out.println("Welcome to my repository");
     }
 }
