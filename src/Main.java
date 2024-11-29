acce33346e8779f44f3df0819f8c9fceebfeba03
PhamVanHuyEIUer <huy.phamvan.cit22@eiu.edu.vn>
2024-11-29 18:15:19 +0700

    update input name

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package src;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Pham Van Huy's Java Reponsitory");
-        System.out.println("Hello Pham Van Huy");
+        System.out.println("Hello <input_name>");
         System.out.println("Welcome to my repository");
     }
 }
